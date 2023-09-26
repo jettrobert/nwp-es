6dd2c22c11c45d0524af9f1d7db0869c858b9b4d
Robert Jett <me@robertjett.com>
2023-09-26 19:46:59 +0200

    Fixed language HTML.

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="es">
         <Head>
           {/* Google Analytics */}
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-TEPCG756FQ"></script>
